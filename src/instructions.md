d48288a89d4a039c0738d9f8819e1ba50fd58244
Wes Reid <bwreid@gmail.com>
2017-02-03 10:00:36 -0800

    Add instructions and tests

---
+++ src/instructions.md
@@ -0,0 +1,43 @@
+## Instructions
+
+For each step below, you should only have to write a single command on the command line. Start the commands below inside of this folder; that is, if you type `pwd` inside this folder you should see the following at the end of output:
+
+```bash
+[...]/git-and-github-basics-assessment/src
+```
+
+### Steps
+
+0. _In a single command_, go back up two directories. You should be outside of the assessment folder.
+
+0. Create a new directory called `git-and-github`.
+
+0. Go into that directory.
+
+0. Initialize a new repository.
+
+0. _In a single command_, create the following files in the following order: `readme.md`, `ignore_me.txt`, and `.gitignore`.
+
+0. Run the following command: `echo ignore_me.txt > .gitignore`
+
+0. Check the status of your git repository.
+
+0. Stage _only_ the `.gitignore` file.
+
+0. Commit this file with a sensible commit message.
+
+0. Stage _all_ of the remaining files.
+
+0. Commit the remaining files with a sensible commit message.
+
+0. Go to github.com and create a new repository. Copy the command to set a new remote named origin and paste it into your terminal.
+
+0. Push to the master branch of origin on your new github repository.
+
+0. _In a single command_, navigate back to the `src/` folder inside of this assessment.
+
+0. Run the following command: `history | tail -14 > my-instructions`
+
+0. Navigate up a directory.
+
+0. Run the tests with the following command: `npm test > ./src/test-output`
---
+++ test/main.js
@@ -0,0 +1,99 @@
+const chai = require('chai')
+const assert = chai.assert
+const fs = require('fs')
+const path = require('path')
+
+describe('cli-commands', () => {
+  let instructions = ''
+  it('has a file called my-instructions', () => {
+    const filePath = path.join('src','my-instructions')
+    instructions = fs.readFileSync(filePath, 'utf-8')
+      .trim().split('\n').map(cmd => cmd.replace(/[0-9]+/,'').trim())
+
+    assert.isOk(instructions)
+  })
+  it('goes up two directories in one command', () => {
+    const cmd = instructions[0]
+    const re = /cd.*\.\.\/\.\./g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('creates a new folder called git-and-github', () => {
+    const cmd = instructions[1]
+    const re = /mkdir.*git-and-github/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('goes into that new directory', () => {
+    const cmd = instructions[2]
+    const re = /cd.*git\-and\-github/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('initializes a new repository', () => {
+    const cmd = instructions[3]
+    const re = /git init/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('creates three new files', () => {
+    const cmd = instructions[4]
+    const re = /touch.*readme\.md.*ignore_me\.txt.*\.gitignore/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('runs the given command', () => {
+    const cmd = instructions[5]
+    const re = /echo ignore_me\.txt > \.gitignore/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('checks the status', () => {
+    const cmd = instructions[6]
+    const re = /git status/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('stages only the .gitignore file', () => {
+    const cmd = instructions[7]
+    const re = /git add.*\.gitignore/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('commits the .gitignore file with a message', () => {
+    const cmd = instructions[8]
+    const re = /git commit \-m/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('stages all other files', () => {
+    const cmd = instructions[9]
+    const re = /git add (-A|\.)/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('commits the other files with a message', () => {
+    const cmd = instructions[10]
+    const re = /git commit \-m/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('creates a new remote', () => {
+    const cmd = instructions[11]
+    const re = /git remote add origin/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('pushes to the master branch', () => {
+    const cmd = instructions[12]
+    const re = /git push \-u origin master/g
+
+    assert.isOk(cmd.match(re))
+  })
+  it('go back to this folder', () => {
+    const cmd = instructions[13]
+    const re = /cd.*\.\.\/git\-and\-github\-basics\-assessment\/src/g
+
+    assert.isOk(cmd.match(re))
+  })
+})
